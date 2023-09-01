32faff6793e94a1faec2c43d9da83fdf7c6cedee
ahmedghazi <atmet.ghazi@gmail.com>
2023-09-01 17:55:27 +0200

    deploy new

--- web/src/app/components/ModalHover.tsx
+++ web/src/app/components/ModalHover.tsx
@@ -10,12 +10,24 @@ const ModalHover = (props: Props) => {
   const [show, setShow] = useState<boolean>(false);
   const [transform, setTransform] = useState<any>({ x: 0, y: 0 });
   const [canTransform, setCanTransform] = useState<boolean>(true);
+  const [iframeLoaded, setFframeLoaded] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement>(null);
   const width = 360,
     height = 740;
 
   useEffect(() => {
+    console.log(window.navigator.userAgent);
     const token = subscribe("PROJECT_IFRAME", (e, d) => {
+      setFframeLoaded(false);
+      // if (ref.current) {
+      //   const iframe = ref.current.querySelector("iframe");
+      //   if (iframe)
+      //     setUserAgent(
+      //       iframe.contentWindow,
+      //       "Mozilla/5.0 (Linux; Android 7.0; SM-G950U Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36"
+      //     );
+      // }
+
       setUrl(d);
     });
 
@@ -68,11 +80,31 @@ const ModalHover = (props: Props) => {
     let clean = u;
     clean = clean.replace("http://", "");
     clean = clean.replace("https://", "");
-    if (clean[clean.length - 1] == "/")
-      clean = clean.substr(0, clean.length - 1);
-    console.log(u, clean);
+    clean = clean.replace(/\/$/, "");
+
     return clean;
   };
+
+  const _onIframeLoaded = () => {
+    setFframeLoaded(true);
+  };
+
+  function setUserAgent(window, userAgent) {
+    if (window.navigator.userAgent != userAgent) {
+      var userAgentProp = {
+        get: function () {
+          return userAgent;
+        },
+      };
+      try {
+        Object.defineProperty(window.navigator, "userAgent", userAgentProp);
+      } catch (e) {
+        window.navigator = Object.create(navigator, {
+          userAgent: userAgentProp,
+        });
+      }
+    }
+  }
 
   return (
     <div
@@ -81,19 +113,15 @@ const ModalHover = (props: Props) => {
         "modal-hover fixed bottom-0 right-xl -translate-x-1/2)- scale-50- pointer-events-none-  will-change-transform transition-opacity- transition-all-  hidden-sm z-50 bg-bg"
       )}
       style={{
-        // transform: !url
-        //   ? `translate(${transform.x}px, ${transform.y}px) scale(0.5)`
-        //   : "scale(0.5)",
-        // transform: `translate(${transform.x}px, ${transform.y}px) scale(0.5)`,
         opacity: show ? 1 : 0,
         width: `${width}px`,
         height: `${height}px`,
         pointerEvents: show ? "all" : "none",
       }}>
-      <div className='header absolute  flex justify-between items-center'>
+      <div className='header sticky  flex justify-between items-center z-50'>
         {url && (
           <a
-            href={url}
+            href={`${url}?platform=mobile`}
             target='_blank'
             rel='noopener noreferrer'
             className='url ellipsis'>
@@ -110,8 +138,13 @@ const ModalHover = (props: Props) => {
           src={url}
           // frameBorder='0'
           // scrolling='no'
+          title='preview'
           width={width}
-          height={height}></iframe>
+          height={height}
+          onLoad={_onIframeLoaded}
+          className={clsx(
+            iframeLoaded ? "opacity-100" : "opacity-10"
+          )}></iframe>
       )}
     </div>
   );
