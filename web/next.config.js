52bf4e56a10f792da86d318c70d64b3f4660d4b2
ahmedghazi <atmet.ghazi@gmail.com>
2024-10-07 14:31:00 +0200

    deploy new

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
+  // output: "out",
+  distDir: "out",
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
